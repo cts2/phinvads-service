8540e74626aa7c462629b9f8f5f4b5d7e376ead4
Kevin Peterson <kevin.peterson@mayo.edu>
2012-08-08 13:54:25 -0500

    Added 'readByTag' functionality.

--- src/main/scala/edu/mayo/cts2/framework/plugin/service/phinvads/profile/codesystemversion/PhinVadsCodeSystemVersionReadService.scala
+++ src/main/scala/edu/mayo/cts2/framework/plugin/service/phinvads/profile/codesystemversion/PhinVadsCodeSystemVersionReadService.scala
@@ -1,20 +1,27 @@
 package edu.mayo.cts2.framework.plugin.service.phinvads.profile.codesystemversion
 
 import java.lang.Override
+
 import scala.collection.JavaConversions._
+
 import org.springframework.stereotype.Component
+
 import edu.mayo.cts2.framework.model.codesystemversion.CodeSystemVersionCatalogEntry
 import edu.mayo.cts2.framework.model.command.ResolvedReadContext
 import edu.mayo.cts2.framework.model.core.VersionTagReference
 import edu.mayo.cts2.framework.model.service.core.NameOrURI
+import edu.mayo.cts2.framework.plugin.service.phinvads.dao.PhinVadsDao
+import edu.mayo.cts2.framework.plugin.service.phinvads.profile.AbstractService
 import edu.mayo.cts2.framework.plugin.service.phinvads.profile.AbstractService
 import edu.mayo.cts2.framework.service.profile.codesystemversion.CodeSystemVersionReadService
 import javax.annotation.Resource
-import edu.mayo.cts2.framework.plugin.service.phinvads.profile.AbstractService
 
 @Component
 class PhinVadsCodeSystemVersionService extends AbstractService with CodeSystemVersionReadService {
 
+  @Resource
+  var phinVadsDao: PhinVadsDao = _
+  
   /**
    * This is incomplete... this is only here to map the 'CURRENT' tag to a CodeSystemVersionName.
    */
@@ -23,7 +30,19 @@ class PhinVadsCodeSystemVersionService extends AbstractService with CodeSystemVe
     codeSystem: NameOrURI,
     tag: VersionTagReference, readContext: ResolvedReadContext): CodeSystemVersionCatalogEntry = {
 
-    null
+    if (tag.getContent() == null || !tag.getContent().equals("CURRENT")) {
+      throw new RuntimeException("Only 'CURRENT' tag is supported")
+    }
+    
+    val csName = codeSystem.getName
+    
+    val id = phinVadsDao.codeSystemIdMaps.codeSystemIdByName.getOrElse(csName, {return null})
+    
+    val entry = new CodeSystemVersionCatalogEntry()
+    
+    entry.setCodeSystemVersionName(id.codeSystemVersionName)
+    
+    entry
   }
 
   @Override
